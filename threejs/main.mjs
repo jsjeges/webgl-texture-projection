cd8de52bbee5d30de5015cbbdd1cdeb19f2a3e1a
James Jeges <jsjeges@gmail.com>
2025-11-29 18:47:20 +1100

    working

--- threejs/main.mjs
+++ threejs/main.mjs
@@ -1,6 +1,7 @@
 import {
   Scene,
   PerspectiveCamera,
+  OrthographicCamera,
   WebGLRenderer,
   BoxGeometry,
   MeshBasicMaterial,
@@ -8,6 +9,7 @@ import {
   Mesh,
   Group,
   Matrix4,
+  AxesHelper,
 } from "./lib/three.module.min.js"
 
 import {makeMaterial} from "./shader.mjs"
@@ -29,12 +31,21 @@ html,body,canvas {
 }
 `)
 
+const keymap = new Map();
+
+const keys = new Proxy(keymap, {get(target,name,receiver){
+  return target.get(name)===true;
+}})
+
 const renderer = new WebGLRenderer();
 
 document.body.appendChild(renderer.domElement);
 
 const scene = new Scene();
 const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const oc = new OrthographicCamera(-2,2,-2,2);
+scene.add(oc)
 
 const cg1 = new Group();
 const cg2 = new Group();
@@ -45,9 +56,12 @@ cg2.rotateX(45*DEG2RAD);
 
 scene.add(cg1);
 
+const ax = new AxesHelper(0.5);
+oc.add(ax);
+
 const geometry = new BoxGeometry( 1, 1, 1 );
 const cube = new Mesh( geometry, material );
-scene.add( cube );
+//scene.add( cube );
 
 cube.translateZ(0.5);
 
@@ -55,7 +69,7 @@ const pg = new PlaneGeometry( 5, 5 );
 const pl = new Mesh( pg, material );
 scene.add( pl );
 
-camera.position.z = 3;
+camera.translateZ(2);
 
 let pFrame = 0
 
@@ -65,18 +79,51 @@ const animation = ()=>{
 
   renderer.setSize( w,h );
   camera.aspect = w/h;
+
   camera.updateProjectionMatrix();
+  oc.updateProjectionMatrix();
 
   const cFrame = performance.now();
   const dTime = (cFrame-pFrame)/1000;
 
   pFrame=cFrame;
+  if(keys.d) cg1.rotateZ((20*dTime)*DEG2RAD)
+  if(keys.a) cg1.rotateZ((-20*dTime)*DEG2RAD)
 
-  renderer.render(scene,camera);
+  if(keys.s) cg2.rotateX((20*dTime)*DEG2RAD)
+  if(keys.w) cg2.rotateX((-20*dTime)*DEG2RAD)
 
-  cg1.rotateZ((27*dTime)*DEG2RAD)
+  if(keys.t) oc.translateZ(1*dTime)
+  if(keys.g) oc.translateZ(-1*dTime)
+
+  if(keys.y) oc.translateX(1*dTime)
+  if(keys.h) oc.translateX(-1*dTime)
+
+  if(keys.b) oc.translateY(1*dTime)
+  if(keys.n) oc.translateY(-1*dTime)
+
+  if(keys.j) oc.rotateZ((20*dTime)*DEG2RAD)
+  if(keys.l) oc.rotateZ((-20*dTime)*DEG2RAD)
+
+  if(keys.k) oc.rotateX((20*dTime)*DEG2RAD)
+  if(keys.i) oc.rotateX((-20*dTime)*DEG2RAD)
+
+
+  material.uniforms.modelViewMatrix2.value.copy(oc.matrixWorldInverse).multiply(pl.matrixWorld)
+  material.uniforms.projectionMatrix2.value.copy(oc.projectionMatrix);
+
+  renderer.render(scene, camera);
 
   requestAnimationFrame(animation)
 }
 
 animation();
+
+
+document.body.onkeydown = (e)=>{
+  keymap.set(e.key,true)
+}
+
+document.body.onkeyup = (e)=>{
+  keymap.set(e.key,false)
+}
--- threejs/shader.mjs
+++ threejs/shader.mjs
@@ -2,6 +2,7 @@ import {
   ShaderMaterial,
   Vector3,
   Vector2,
+  Matrix4,
   TextureLoader
 } from "./lib/three.module.min.js"
 
@@ -10,14 +11,19 @@ const t1 = textureLoader.load( 'cloud.png' );
 const t2 = textureLoader.load(  'target.png' );
 
 const vertexShader=`
+uniform mat4 modelViewMatrix2;
+uniform mat4 projectionMatrix2;
+uniform mat4 worldMatrix;
+
 varying vec2 vUv;
+varying vec4 viewPosition;
 
 void main() {
 
-  vUv = uv;
-  vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
-  gl_Position = projectionMatrix * mvPosition;
+  viewPosition = projectionMatrix2 * modelViewMatrix2 * vec4(position, 1.0f);
 
+  vUv = uv;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
 }
 `
 
@@ -27,20 +33,26 @@ uniform sampler2D texture1;
 uniform sampler2D texture2;
 
 varying vec2 vUv;
+varying vec4 viewPosition;
 
-void main( void ) {
+void main( ) {
+
+  vec2 projectTexCoord;
+  projectTexCoord.x = viewPosition.x / viewPosition.w / 2.0f + 0.5f;
+  projectTexCoord.y = viewPosition.y / viewPosition.w / 2.0f + 0.5f;
 
   vec4 t1 = texture2D( texture1, vUv );
-  vec4 t2 = texture2D( texture2, vUv );
+  vec4 t2 = texture2D( texture2, projectTexCoord );
 
   gl_FragColor = vec4(t1.r, t2.r, 1.0, 1.0);
 }
 `
 
 const uniforms = {
-  'time': { value: 1.0 },
   'texture1': { value: t1 },
-  'texture2': { value: t2 }
+  'texture2': { value: t2 },
+  'projectionMatrix2':{value: new Matrix4()},
+  'modelViewMatrix2':{value: new Matrix4()},
 };
 
 export const makeMaterial= ()=>{
